04c47987fcee8d26521e158725d74dcb3fe92292
Child of the Universe <childoftheuniverse@protonmail.com>
2019-09-30 21:51:36 +0200

    Initialize Prometheus and OpenCensus for the servers.

--- bin/content_server/main.go
+++ bin/content_server/main.go
@@ -3,18 +3,30 @@ package main
 import (
 	"crypto/tls"
 	"flag"
+	"fmt"
 	"log"
+	"net/http"
 	"os"
 	"os/signal"
 	"strings"
 	"syscall"
 	"time"
 
+	"contrib.go.opencensus.io/exporter/prometheus"
+	"contrib.go.opencensus.io/exporter/zipkin"
 	"github.com/childoftheuniverse/cstatic/server"
 	_ "github.com/childoftheuniverse/filesystem-file"
 	rados "github.com/childoftheuniverse/filesystem-rados"
+	openzipkin "github.com/openzipkin/zipkin-go"
+	openzipkinModel "github.com/openzipkin/zipkin-go/model"
+	zipkinReporter "github.com/openzipkin/zipkin-go/reporter"
+	zipkinHTTP "github.com/openzipkin/zipkin-go/reporter/http"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	etcd "go.etcd.io/etcd/clientv3"
 	"go.etcd.io/etcd/pkg/transport"
+	"go.opencensus.io/plugin/ocgrpc"
+	"go.opencensus.io/stats/view"
+	"go.opencensus.io/trace"
 )
 
 /*
@@ -40,6 +52,9 @@ func main() {
 	var etcdServerList string
 	var etcdTimeout time.Duration
 	var etcdConfig etcd.Config
+	var zipkinEndpoint string
+	var thisHost string
+	var traceProbability float64
 
 	var privateKeyPath string
 	var publicKeyPath string
@@ -50,15 +65,27 @@ func main() {
 	var client *etcd.Client
 	var tlsConfig *tls.Config
 	var tlsInfo transport.TLSInfo
+	var pe *prometheus.Exporter
 	var err error
+
+	if thisHost, err = os.Hostname(); err != nil {
+		log.Printf("Warning: cannot determine host name: %s", err)
+	}
+
+	flag.StringVar(&zipkinEndpoint, "zipkin-endpoint",
+		fmt.Sprintf("%s:9411", thisHost),
+		"host:port pair to send Zipkin traces to")
+	flag.Float64Var(&traceProbability, "trace-probability", -1.0,
+		"If set to a value above 0, probability for traces to be sent")
 
 	flag.StringVar(&configPath, "config", "",
 		"Path to config file inside etcd")
-	flag.StringVar(&etcdServerList, "etcd-servers", "",
+	flag.StringVar(&etcdServerList, "etcd-servers",
+		fmt.Sprintf("%s:2379", thisHost),
 		"Comma separated list of etcd servers to contact")
 	flag.DurationVar(&etcdTimeout, "etcd-timeout", 2*time.Second,
 		"Maximum amount of time to wait for etcd server connection")
-	flag.StringVar(&bindAddress, "bind", "[::]:0",
+	flag.StringVar(&bindAddress, "bind", fmt.Sprintf("%s:0", thisHost),
 		"Address to bind to in order to serve requests")
 
 	/* TLS configuration */
@@ -70,6 +97,47 @@ func main() {
 		"Path to a server CA certificate file. If unset, servers will not be "+
 			"authenticated.")
 	flag.Parse()
+
+	if err = view.Register(ocgrpc.DefaultClientViews...); err != nil {
+		log.Fatal("Error registering default gRPC client views: ", err)
+	}
+	if err = view.Register(ocgrpc.DefaultServerViews...); err != nil {
+		log.Fatal("Error registering default gRPC server views: ", err)
+	}
+
+	if pe, err = prometheus.NewExporter(prometheus.Options{
+		Namespace: "cstatic_content_server",
+	}); pe != nil {
+		log.Fatal("Error creating prometheus exporter: ", err)
+	}
+	http.Handle("/metrics", promhttp.Handler())
+	http.Handle("/metrics-grpc", pe)
+
+	if zipkinEndpoint != "" {
+		var localEndpoint *openzipkinModel.Endpoint
+		var reporter zipkinReporter.Reporter
+		var zipkinExporter trace.Exporter
+
+		localEndpoint, err = openzipkin.NewEndpoint(
+			"cstatic_content_server", bindAddress)
+		if err != nil {
+			log.Fatalf("Failed to create the local zipkin endpoint: %s", err)
+		}
+		reporter = zipkinHTTP.NewReporter(fmt.Sprintf("http://%s/api/v2/spans",
+			zipkinEndpoint))
+		zipkinExporter = zipkin.NewExporter(reporter, localEndpoint)
+		trace.RegisterExporter(zipkinExporter)
+
+		if traceProbability >= 1.0 {
+			trace.ApplyConfig(trace.Config{
+				DefaultSampler: trace.AlwaysSample(),
+			})
+		} else if traceProbability >= 0.0 {
+			trace.ApplyConfig(trace.Config{
+				DefaultSampler: trace.ProbabilitySampler(traceProbability),
+			})
+		}
+	}
 
 	/* Cancel etcd watcher when we stop the process. */
 	signal.Notify(sigChannel, syscall.SIGINT, syscall.SIGTERM)
